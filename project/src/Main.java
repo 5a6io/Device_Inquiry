35c2f4f044826ce9125a4bbc267f6110b6b5750f
YunHye-Mi <yhm54321@naver.com>
2023-05-03 01:55:41 +0900

    데이터베이스시스템과제

--- project/src/Main.java
+++ project/src/Main.java
@@ -6,12 +6,13 @@ public class Main extends JFrame {
 
     public Main() {
         String[] b1_list = {};
+        String[] b2_list = {};
         JComboBox b1 = new JComboBox<>(b1_list);
-//        JComboBox b2 = new JComboBox<>(b2_list);
+        JComboBox b2 = new JComboBox<>(b2_list);
         JPanel p1 = new JPanel();
         JPanel p2 = new JPanel();
 
-        setTitle("공공의료기관 조회");
+        setTitle("영화 정보 조회");
         setSize(700, 600);
 
         setLayout(null);
@@ -19,12 +20,10 @@ public class Main extends JFrame {
         p2.setBounds(0, 50, 700, 500);
 
         p1.add(b1);
-//        p1.add(b2);
+        p1.add(b2);
 
         p1.setLayout(new FlowLayout());
 
-
-        p2.setLayout(new GridLayout(300, 3));
 
         add(p1);
         add(p2);
@@ -32,16 +31,17 @@ public class Main extends JFrame {
         setVisible(true);
 
         try{
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Medical?" +
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Movie?" +
                     "useUnicode=true&useJDBCCompliantTimezoneShift=true?useLegacyDatetimeCode=false" +
                     "&serverTimezone=UTC", "root", "pubugi0314");
 
             Statement stmt = connection.createStatement();
-            String s = "";
+            String s = "select * from movies";
 
             ResultSet resultSet = stmt.executeQuery(s);
 
             while (resultSet.next()) {
+
             }
 
             b1.addActionListener(e -> {
