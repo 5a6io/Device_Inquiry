192a479d9d4f95b792f27e82dd5cf5e3f4b83178
YunHye-Mi <yhm54321@naver.com>
2023-05-13 19:34:42 +0900

    테이블 생성 기능 구현 완료

--- Movie_Inquiry/src/Main.java
+++ Movie_Inquiry/src/Main.java
@@ -1,7 +1,66 @@
+import java.io.FileOutputStream;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
+    private static Connection conn;
+    static Scanner scanner = new Scanner(System.in);
+    static int numRecords = 10000000;
+
+
+    static void createTable(String s) {
+        try {
+            String table = "";
+            System.out.println("컬럼과 도메인을 순서대로 입력하세요:");
+
+            while (true) {
+                String column = scanner.next() + " ";
+                String domain = scanner.next().toUpperCase() + ",";
+                table += column;
+                table += domain;
+                System.out.println("다른 컬럼이 있다면 1번 아니면 2번");
+                int select = scanner.nextInt();
+                if (select == 2) {
+                    break;
+                }
+            }
+
+            String create = "CREATE TABLE IF NOT EXISTS "+ s + "(id INT NOT NULL AUTO_INCREMENT," +
+                     table +
+                    "PRIMARY KEY(id))";
+
+            PreparedStatement pstmt = conn.prepareStatement(create);
+            pstmt.execute();
+
+            return;
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e);
+        }
+    }
+
+    static void insert(String s){
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(s);
+
+            String insert = "INSERT INTO "+ s + "(title, genre, runtime, year, rating, actors, rate, release_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+            DatabaseMetaData metaData = conn.getMetaData();
+            ResultSet resultSet = metaData.getColumns(null, null, s, null);
+
+            while (resultSet.next()) {
+                String dataType = resultSet.getString("DATA_TYPE");
+//                if (dataType.equals(""))
+            }
+
+            pstmt.executeUpdate();
+            pstmt.close();
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e);
+        }
+    }
     public static void main(String[] args) {
 
         try {
@@ -10,10 +69,7 @@ public class Main {
             String url = "jdbc:mysql://localhost:3306/";
             String user = "root";
             String password = "pubugi0314";
-            Connection conn = DriverManager.getConnection(url, user, password);
-            DatabaseMetaData dbm = conn.getMetaData();
-            ResultSet schemas = dbm.getSchemas();
-            boolean existSchema = false;
+            conn = DriverManager.getConnection(url, user, password);
 
             // 스키마 생성
             String schemaName = "Movie_Inquiry";
@@ -23,91 +79,39 @@ public class Main {
             pstmt.close();
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + schemaName, user, password);
 
-            // 테이블 생성
-            String tableName = "movies";
-            String create = "CREATE TABLE IF NOT EXISTS "+ tableName + "(" +
-                        "serial_num INT NOT NULL AUTO_INCREMENT," +
-                        "title VARCHAR(45) NOT NULL," +
-                        "genre VARCHAR(5)," +
-                        "runtime INT," +
-                        "year INT," +
-                        "rating NUMERIC(3,2)," +
-                        "actors VARCHAR(45)," +
-                        "rate VARCHAR(7)," +
-                        "release_status BOOLEAN NOT NULL," +
-                        "PRIMARY KEY(serial_num, title)," +
-                        "INDEX idx_movies_serial(serial_num) USING BTREE)";
-
-            pstmt = conn.prepareStatement(create);
-            pstmt.execute();
-
-            // 데이터 삽입
-            String insert = "INSERT INTO movies (title, genre, runtime, year, rating, actors, rate, release_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-            pstmt = conn.prepareStatement(insert);
-
-            Scanner scanner = new Scanner(System.in);
-
             while (true) {
-                System.out.println("영화정보입력:");
-                String title = scanner.next();
-                if (title.equals("break")) {
-                    break;
-                }
-                pstmt.setString(1, title);
+                System.out.println("1.테이블 생성\n2.레코드 삽입\n3.질의처리");
+                String tableName = null;
+                if (scanner.next().equals("1")) {
+                    tableName = scanner.next();
+                    createTable(tableName);
+                } else if (scanner.next().equals("2")) {
+                    // 데이터 삽입
+                    tableName = scanner.next();
+                    insert(tableName);
 
-                String genre = scanner.next();
-                if (genre.equals("null")) {
-                    pstmt.setNull(2, Types.CHAR);
+                    DatabaseMetaData metaData = conn.getMetaData();
+                    metaData.getColumns(null, null, tableName, null);
+
+                    ResultSet resultSet = pstmt.getResultSet();
+
+                    int[] genreBitmap = new int[numRecords / 32 + 1];
+                    int[] releaseBitmap = new int[numRecords / 32 + 1];
+
+                    FileOutputStream fileOutputStream;
+
+
+                } else if (scanner.next().equals("3")) {
+
                 } else {
-                    pstmt.setString(2, genre);
+                    conn.close();
+                    return;
                 }
 
-                int runtime = scanner.nextInt();
-                if (runtime == 0) {
-                    pstmt.setNull(3, Types.INTEGER);
-                } else {
-                    pstmt.setInt(3, runtime);
-                }
-
-                int year = scanner.nextInt();
-                if (year == 0) {
-                    pstmt.setNull(4, Types.INTEGER);
-                } else {
-                    pstmt.setInt(4, year);
-                }
-
-                double rating = scanner.nextDouble();
-                if (rating == 0) {
-                    pstmt.setNull(5, Types.DOUBLE);
-                } else {
-                    pstmt.setDouble(5, rating);
-                }
-
-                String actors = scanner.next();
-                if (actors.equals("null")) {
-                    pstmt.setNull(6, Types.CHAR);
-                } else {
-                    pstmt.setString(6, actors);
-                }
-
-                String rate = scanner.next();
-                if (rate.equals("null")) {
-                    pstmt.setNull(7, Types.CHAR);
-                } else {
-                    pstmt.setString(7, rate);
-                }
-
-                boolean releaseStatus = scanner.nextBoolean();
-                pstmt.setBoolean(8, releaseStatus);
-
-                pstmt.executeUpdate();
             }
 
-            pstmt.close();
-            conn.close();
-
         } catch (SQLException e) {
-            System.out.println("SQLExeption: " + e);
+            System.out.println("SQLException: " + e);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
